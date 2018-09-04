cd63754cc9566382b875360d13a90cc14f7c0c1b
Zubair <zubair.shokh@gmail.com>
2018-09-04 14:35:32 +0530

    changed httppoison version

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Geonames.Mixfile do
   defp deps do
     [
       { :poison, "~> 3.0" },
-      { :httpoison, "~> 0.11.0" },
+      {:httpoison, "~> 1.0"},
       { :ex_doc, "~> 0.12", only: :dev }
     ]
   end
